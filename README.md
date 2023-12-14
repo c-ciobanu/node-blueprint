eb25d7873f39774b24c5ed4e16375675ca17b4f8
Cristi Ciobanu <c-ciobanu@protonmail.com>
2023-12-14 17:14:28 +0100

    add prompt for the project name

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Local development
 
 ```bash
-# Stubs dist once removing the need to watch and rebuild during development
+# stubs dist once removing the need to watch and rebuild during development
 pnpm dev
 # executes the CLI
 node index
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,64 @@
-const message: string = "Hello, World!";
-console.log(message);
+import fs from "node:fs";
+import prompts from "prompts";
+import kleur from "kleur";
+
+const defaultTargetDir = "node-blueprint";
+
+async function init() {
+  let result: prompts.Answers<"targetDir">;
+
+  try {
+    result = await prompts(
+      [
+        {
+          type: "text",
+          name: "targetDir",
+          message: "What is the name of your project?",
+          initial: defaultTargetDir,
+          format: formatTargetDir,
+        },
+        {
+          type: (targetDir: string) => {
+            if (!isEmptyDir(targetDir)) {
+              throw new Error(
+                `${kleur.red("✖")} Directory ${kleur.bold(
+                  targetDir
+                )} already exists and is not empty. Please empty the directory or choose a different name`
+              );
+            }
+
+            return null;
+          },
+          name: "",
+        },
+      ],
+      {
+        onCancel: () => {
+          throw new Error(`${kleur.red("✖")} Operation cancelled`);
+        },
+      }
+    );
+  } catch (cancelled: any) {
+    console.error(cancelled.message);
+    return;
+  }
+
+  console.log(result);
+}
+
+function formatTargetDir(targetDir: string) {
+  return targetDir.trim().replace(/\/+$/g, "");
+}
+
+function isEmptyDir(dirName: string) {
+  if (!fs.existsSync(dirName)) {
+    return true;
+  }
+
+  const files = fs.readdirSync(dirName);
+  return files.length === 0;
+}
+
+init().catch((e) => {
+  console.error(e);
+});
